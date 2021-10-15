f24865468238af21868c29524a373f250360f6ad
Leonardo da Silva Sales Borges <66486368+LeonardoSBorges@users.noreply.github.com>
2021-10-15 11:50:11 -0300

    Exercício (Herança e Polimorfismo)
    Fazer um programa para ler os dados de N produtos (N fornecido pelo usuário). Ao final, mostrar a etiqueta de preço de cada produto na mesma ordem em que foram digitados.
    Todo produto possui nome e preço. Produtos importados possuem uma taxa de alfândega, e produtos usados possuem data de fabricação. Estes dados específicos devem ser acrescentados na etiqueta de preço conforme o exemplo (que estava na vídeo aula). Para produtos importados, a taxa e alfândega deve ser acrescentada ao final do produto.

---
+++ Produtos/Produtos/Entities/ImportedProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produtos.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+        public ImportedProduct(string name, double price, double custumsFee) : base(name, price)
+        {
+            CustomsFee = custumsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} $ {TotalPrice().ToString("F2", CultureInfo.InvariantCulture)} (Customs fee: $ {CustomsFee.ToString("F2", CultureInfo.InvariantCulture)})";
+        }
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+    }
+}
